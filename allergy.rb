47bf17faf8f4b1c6e57111c530718fafa4e3cc34
Larisasmiles <larisasmiles@gmail.com>
2017-05-17 09:26:43 -0600

    updated

--- allergy.rb
+++ allergy.rb
@@ -14,38 +14,29 @@
 #        Because you all love TDD, here are some cases that you can test against:
 #        * A score of two would mean that the patient is allergic to just peanuts.
 #        * A score of 3 would mean that the patient is allergic to eggs and peanuts.”
-# require "pry"
 
-# def allergy_score(score)
-#   new_allergy_array = []
-#   allergies = {128 => 'cats', 64 => 'pollen', 32 => 'chocolate', 16 => 'tomatoes', 8 => 'strawberries', 4 => 'shellfish', 2 => 'peanuts', 1 => 'eggs'}
-#
-#   allergies.each do |number, allergy|
-#     if score - number >= 0
-#       new_allergy_array << allergy
-#       score = score - number
-#     end
-#   end
-#     new_allergy_array
-# end
 require "pry"
 class Allergy
+  attr_reader :score
 
-  def allergy_list
-      allergies = {
-        "eggs" => 1, "peanutes" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16, "chocolate" => 32, "pollen" => 64,
-        "cats" => 128 }
+  def initialize(score)
+    @score              = score
+    multiple_allergies = []
+    allergy_list       = {
+      "eggs" => 1, "peanutes" => 2, "shellfish" => 4, "strawberries" => 8, "tomatoes" => 16, "chocolate" => 32, "pollen" => 64,
+      "cats" => 128 }
   end
 
   def adding_allergies(score)
-    multiple_allergies = []
-    allergy_list.each do |num, allergy|
-      if score - num >= 0
+    allergy_list.each do |num, allergy| #look into .map...
+      if @score - num >= 0
         multiple_allergies << allergy
-        score = score - num
+        @score = score - num
         binding.pry
       end
   end
       multiple_allergies
   end
 end
+
+a = Allergy.new(3)
